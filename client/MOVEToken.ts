c4a6f6ae2adcbf79ee05f75567aa8c288b01e377
danielbui12 <huytung139@gmail.com>
2024-04-24 18:58:28 +0700

    update spl token metadata

--- client/MOVEToken.ts
+++ client/MOVEToken.ts
@@ -3,6 +3,8 @@ import { ASSETS } from "../tests/util/const"
 import { mintNewTokens } from "../tests/util/token"
 import { getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
 import { connection, payer } from "./utils";
+import { createCreateMetadataAccountV3Instruction, PROGRAM_ID as METADATA_PROGRAM_ID } from "@metaplex-foundation/mpl-token-metadata";
+import { buildTransaction } from "../tests/util/transaction";
 
 const target = new PublicKey("FmWhTtXMgYTfmR53xUwGn3ZNGeKT8hySQPfpDCC46k8R");
 
@@ -13,6 +15,49 @@ async function createToken() {
         ASSETS[0][6],
         ASSETS[0]
     )
+}
+
+async function createTokenMetadata(mint: PublicKey) {
+    const tx = await buildTransaction(
+        connection,
+        payer.publicKey,
+        [payer],
+        [createCreateMetadataAccountV3Instruction({
+            metadata: PublicKey.findProgramAddressSync(
+                [
+                    Buffer.from('metadata'),
+                    METADATA_PROGRAM_ID.toBuffer(),
+                    mint.toBuffer(),
+                ],
+                METADATA_PROGRAM_ID
+            )[0],
+            mint: mint,
+            mintAuthority: payer.publicKey,
+            payer: payer.publicKey,
+            updateAuthority: payer.publicKey,
+        },
+        {
+            createMetadataAccountArgsV3: {
+                data: {
+                    name: ASSETS[0][0],
+                    symbol: ASSETS[0][0],
+                    uri: ASSETS[0][3],
+                    creators: null,
+                    sellerFeeBasisPoints: 0,
+                    uses: null,
+                    collection: null,
+                },
+                isMutable: false,
+                collectionDetails: null,
+            },
+        })]
+    );
+    const txSig = await connection.sendTransaction(tx);
+    const ltsBlock = await connection.getLatestBlockhash();
+    await connection.confirmTransaction({
+        ...ltsBlock,
+        signature: txSig,
+    })
 }
 
 async function mintToken(mint: PublicKey) {
@@ -26,9 +71,12 @@ async function mintToken(mint: PublicKey) {
         ASSETS[0][5] * 10 ** ASSETS[0][4],
     )
 }
+
 async function main() {
     // await createToken();
-    // await mintToken(new PublicKey("ASdZagtrNjFoFTfd1kDKeFxFgbPenuFbepd1EVYXsyt4"));
+    // const mint = new PublicKey("ASdZagtrNjFoFTfd1kDKeFxFgbPenuFbepd1EVYXsyt4");
+    // await createTokenMetadata(mint);
+    // await mintToken(mint);
 }
 
 main().then(() => {
